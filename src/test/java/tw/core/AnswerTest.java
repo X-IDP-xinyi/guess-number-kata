18c96d6d9518c8afcf8b4c8b804eb140ae8ab5e8
Andrea <Andrea2000728>
2023-02-21 21:00:24 +0800

    feat : finish the AnswerTest

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,16 +2,49 @@ package tw.core;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by jxzhong on 2017/9/23.
  */
-public class AnswerTest {
+class AnswerTest {
     private Answer actualAnswer;
 
     @BeforeEach
     public void setUp() {
         actualAnswer = Answer.createAnswer("1 2 3 4");
     }
+
+    @Test
+    void should_return_answer_when_createAnswer_input_is_inputStr() {
+        actualAnswer = Answer.createAnswer("1 2 3 4");
+        assertThat(actualAnswer.toString(), is("1 2 3 4"));
+    }
+
+    @Test
+    void should_validate_answer_when_createAnswer_input_is_correct() {
+        actualAnswer = Answer.createAnswer("1 2 3 4");
+        try {
+            actualAnswer.validate();
+        } catch (AnswerFormatIncorrectException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void should_invalidate_answer_when_createAnswer_input_is_false() {
+        actualAnswer = Answer.createAnswer("1 2 12 4");
+        try {
+            actualAnswer.validate();
+        } catch (AnswerFormatIncorrectException e) {
+//            throw new RuntimeException(e);
+            System.out.println("Throw AnswerFormatIncorrectException");
+        }
+    }
+
 
 }
